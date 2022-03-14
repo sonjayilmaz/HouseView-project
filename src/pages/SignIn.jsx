78462559442b84f44b7f02ed1e7514fbcbc1692d
sonjayilmaz <sonja_yilmaz@hotmail.com>
2022-03-14 14:19:33 +0100

    Added form in SignIn

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,9 +1,58 @@
+import {useState} from 'react'
+import {Link, useNavigate} from 'react-router-dom'
+import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
+import visibilityIcon from '../assets/svg/visibilityIcon.svg'
+
 function SignIn() {
-    return (
-        <div>
-     <h1>Sign In</h1>
-     </div>
-    );
+  const [showPassword, setShowPassword] = useState(false)
+  const [formData, setFormData] = useState({
+    email: '',
+    password: ''
+  })
+  const {email, password} = formData
+
+  const navigate = useNavigate()
+
+  const onChange = () => {
+
   }
+
+    return (
+      <>
+      <div className='pageCotainer'>
+      <header>
+        <p className='pageHeader'>
+          Welcome Back!
+        </p>
+      </header>
+      <form>
+        <input type='email' className='emailInput' 
+        placeholder='Email'
+        id='email'
+        value={email}
+        onChange={onChange}
+        />
+
+        <div className='passwordInputDiv'>
+          <input type={showPassword ? 'text' : 'password'}
+          className='passwordInput'
+          placeholder='Password'
+          id='password'
+          value={password}
+          onChange={onChange}
+          />
+
+          <img src={visibilityIcon} 
+          alt='show password'
+          className='showPassword'
+          onClick={() => setShowPassword((prevState) => 
+           !prevState)}
+          />
+        </div>
+      </form>
+      </div>
+      </>
+    )
+}
   
   export default SignIn;

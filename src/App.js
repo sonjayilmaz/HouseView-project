ce33a462860b969142854b41b3e5b9dae1ed0e1f
sonjayilmaz <sonja_yilmaz@hotmail.com>
2022-06-15 17:03:04 +0200

    Added react-toastify for popup

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { BrowserRouter as Router, Routes, Route }
 from 'react-router-dom'
+import {ToastContainer} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 import Navbar from './components/Navbar';
 import Explore from './pages/Explore';
 import Offers from './pages/Offers'
@@ -25,6 +27,8 @@ function App() {
     </Routes>
     <Navbar />
     </Router>
+
+    <ToastContainer />
     </>
   );
 }

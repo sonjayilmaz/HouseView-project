a34a2d4a492ebdc916bf1a046c76d6679c1cfe9b
sonjayilmaz <sonja_yilmaz@hotmail.com>
2022-06-16 13:33:18 +0200

    Updated Profile.jsx

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,18 +1,73 @@
 import { useState, useEffect } from 'react';
 import {getAuth} from 'firebase/auth'
+import {updateDoc} from 'firebase/firestore'
+import {db} from '../firebase.config'
+import {useNavigate, Link} from 'react-router-dom' 
 
 
 function Profile() {
-  const [user, setUser] = useState(null)
-
-
   const auth = getAuth()
-  useEffect(() =>{
-  setUser(auth.currentUser)
-  }, [])
+  const [changeDetails, setChangeDetails] = useState(false)
+  const [formData, setFormData] = useState({
+   name: auth.currentUser.displayName,
+   email: auth.currentUser.email,
+  })
+
+const { name, email } = formData
+
+const navigate = useNavigate()
+  
+  const onLogout = () => {
+auth.signOut()
+navigate('/')
+  }
+
+  const onSubmit = () => {
+    console.log(123);
+  }
+
+  const onChange = (e) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [e.target.id]: e.target.value,
+    }))
+  }
 
 
-    return user ? <h1>{user.displayName}</h1> : 'Not Logged In'
+    return <div className='profile' >
+      <header className='profileHeader'>
+        <p className='pageHeader'>My Profile</p>
+        <button type='button' className='logOut' onClick={onLogout}
+        >
+          Log out
+        </button>
+         </header>
+
+         <main>
+           <div className='profileDetailsHeader'>
+             <p className='profileDetailsText'>Personal Details</p>
+             <p className='changePersonalDetails' onClick={() => {
+               changeDetails && onSubmit()
+               setChangeDetails((prevState) => !prevState)
+             }}>
+               {changeDetails ? 'done' : 'change'}
+             </p>
+           </div>
+
+           <div className='profileCard'>
+             <form>
+               <input 
+               type='text' 
+               id='name'
+               className={!changeDetails ? 'profileName' : 'profileNameActive'}
+               disabled={!changeDetails}
+               value={name}
+               onChange={onChange}
+               />
+             </form>
+           </div>
+         </main>
+    </div>
       
   }
   

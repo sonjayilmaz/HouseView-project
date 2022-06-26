d28c1725397ff994629e77f041683428a18d107e
sonjayilmaz <sonja_yilmaz@hotmail.com>
2022-06-26 16:44:07 +0200

    Added create listing page

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import Profile from './pages/Profile';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
+import CreateListing from './pages/CreateListing';
 
 
 
@@ -29,6 +30,7 @@ function App() {
     <Route path='/sign-in' element={<SignIn/>}/>
     <Route path='/sign-up' element={<SignUp/>}/>
     <Route path='/forgot-password' element={<ForgotPassword/>}/>
+    <Route path='/create-listing' element={<CreateListing/>}/>
     </Routes>
     <Navbar />
     </Router>

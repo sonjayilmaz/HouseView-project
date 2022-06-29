d5bcef9d671856d9957911b22bf0f26b05f54f60
sonjayilmaz <sonja_yilmaz@hotmail.com>
2022-06-29 13:32:08 +0200

    Showing listing details and added contact page

--- src/App.js
+++ src/App.js
@@ -12,6 +12,8 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import ForgotPassword from './pages/ForgotPassword';
 import CreateListing from './pages/CreateListing';
+import Listing from './pages/Listing';
+import Contact from './pages/Contact';
 
 
 
@@ -31,6 +33,8 @@ function App() {
     <Route path='/sign-up' element={<SignUp/>}/>
     <Route path='/forgot-password' element={<ForgotPassword/>}/>
     <Route path='/create-listing' element={<CreateListing/>}/>
+    <Route path='/category/:categoryName/:listingId' element={<Listing/>}/>
+    <Route path='/contact/:landlordId' element={<Contact/>}/>
     </Routes>
     <Navbar />
     </Router>
